a4826bbf7347a0b72fc4dc27c3fce54c50349c21
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-02-22 18:39:07 +0100

    ultimo ok

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ export const PORT = process.env.PORT || 3150
 
 export const DB_HOST = process.env.DB_HOST || "192.168.1.147"
 export const DBPORT = process.env.DBPORT || 3306
-export const DB_USER = process.env.DB_USER || "sulbaranjc"
-export const DB_PASSWORD = process.env.DB_PASSWORD || "4688"
+export const DB_USER = process.env.DB_USER || "root"
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Jc10439536+"
 export const DB_DATABSES = process.env.DB_DATABSES || "images"
 
  
